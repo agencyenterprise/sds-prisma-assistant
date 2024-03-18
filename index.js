dcb54f62abdf1080a4458ddd5657bb043aca7ed2
Adão Pardim Jr <adao.pardim@ae.studio>
2024-03-18 11:10:52 -0300

    Add command to run Prisma Assist

--- index.js
+++ index.js
@@ -1,25 +1,48 @@
+#!/usr/bin/env node
+
 const next = require('next')
 const path = require('path')
 const { parse } = require('url')
 const { createServer } = require('http')
+const { program } = require('commander')
 
-const app = next({
-  dev: false,
-  customServer: true,
-})
-
-const handle = app.getRequestHandler()
-
-app.prepare().then(() => {
-  process.env['SCHEMA_PATH'] = getSchemaPath()
-  createServer((req, res) => {
-    const parsedUrl = parse(req.url, true)
-    handle(req, res, parsedUrl)
-  }).listen(3007, () => {
-    console.log('> Ready on http://localhost:3007')
-  })
-})
-
-function getSchemaPath() {
-  return path.resolve(__dirname, './schemas/stocks/schema.prisma')
+function getSchemaPath(schema) {
+  return path.resolve(__dirname, schema)
 }
+
+program
+  .version('0.0.1')
+  .description('Chat with your schema with Prisma Assist')
+  .option('--schema <path>', 'Custom path to your Prisma schema')
+  .option('-p, --port <number>', 'Port to start Prisma Assist on')
+  .action((options) => {
+    const port = options.port || 4500
+    const schema = options.schema || './prisma/schema.prisma'
+
+    const app = next({
+      dev: false,
+      customServer: true,
+    })
+
+    const handle = app.getRequestHandler()
+
+    app.prepare().then(() => {
+      process.env['SCHEMA_PATH'] = getSchemaPath(schema)
+      createServer(async (req, res) => {
+        await handle(req, res, parse(req.url, true))
+      }).listen(port, () => {
+        console.log(`> Ready on http://localhost:${port}`)
+      })
+    })
+  })
+
+program.addHelpText(
+  'after',
+  `
+
+Examples:
+  $ npx prisma-assist --schema=./schema.prisma
+`,
+)
+
+program.parse(process.argv)
