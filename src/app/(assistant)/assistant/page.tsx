a964675f1244bd4a7e562ce77813cd7bc31f52a8
Adão Pardim Jr <adao.pardim@ae.studio>
2024-03-21 14:02:42 -0300

    Update metadata

--- src/app/(assistant)/assistant/page.tsx
+++ src/app/(assistant)/assistant/page.tsx
@@ -1,11 +1,5 @@
-import { Metadata } from 'next'
-
 import { Chat } from '@/components/chat'
 import { nanoid } from '@/lib/utils'
-
-export const metadata: Metadata = {
-  title: 'Prisma Assistant',
-}
 
 export default function IndexPage() {
   const id = nanoid()
--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,13 +1,8 @@
-import { Metadata } from 'next'
 import { redirect } from 'next/navigation'
 import React from 'react'
 
 import Homepage from '@/components/homepage'
 import { config } from '@/lib/config'
-
-export const metadata: Metadata = {
-  title: 'Prisma Assistant',
-}
 
 export default function Home() {
   if (!config.isWebsiteMode) {
